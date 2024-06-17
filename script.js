3495cf3c5d2da3c983996fc85aecc66bd8f8dc54
Involentary-Pterodacyl <153659214+Involentary-Pterodacyl@users.noreply.github.com>
2024-06-17 17:23:49 +0000

    actually does math now

--- script.js
+++ script.js
@@ -64,72 +64,10 @@ b8.onclick = function() {
 b9.onclick = function() {
     addNum("9")
 }
+clear.onclick = function() {
+    p.innerText = ""
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const add = function(num1, num2) {
-
-//     return num1 + num2
-// };
-
-// const subtract = function(num1, num2) {
-
-//   return num1-num2
-// };
-
-// const sum = function(array) {
-//     len = array.length
-//   let finalNum = 0
-//   for (let x = 0; x < len; x++) {
-//     finalNum += array.at(x)
-//   }
-
-//   return finalNum
-// };
-
-// const multiply = function(array) {
-//   len = array.length
-//   let finalNum = array.at(0) * array.at(1)
-//   if (len < 3) {
-//     return finalNum
-//   } else {
-//   for (let x = 2; x < len; x++) {
-//     finalNum *= array.at(x)
-//   }
-//   console.log(finalNum)
-//   return finalNum
-// }
-
-// };
-
-// const power = function(num1, num2) {
-//   let finalNum = num1
-//     for (let x = 1; x < num2; x++) {
-//     finalNum *= num1
-//   }
-
-//   return finalNum
-// };
-
-// const factorial = function(num) {
-//   let finalNum = 1
-//     if (num == 0) {
-//     return finalNum
-//   } else {
-//     for( let x = 2; x <= num; x++){
-//       finalNum *= x
-//     }
-//     return finalNum
-//   }
-// };
+equal.onclick = function() {
+    p.innerText = eval(p.innerText);
+}
